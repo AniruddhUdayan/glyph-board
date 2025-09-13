f7234a658de065f4e568c00e48c0d8e9bb99e11e
Aniruddh Languify <aniruddh.languify@gmail.com>
2025-09-13 11:41:54 +0530

    feat():added db package

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -13,13 +13,10 @@ wss.on("connection", (ws , request) => {
     }
 
     const queryParams = new URLSearchParams(url.split("?")[1]);
-    const token = queryParams.get("token");
-    if(!token) {
-        ws.close();
-        return;
-    }
+    const token = queryParams.get("token") || "";
+
     const decoded = jwt.verify(token, JWT_SECRET) as { userId: number };
-    if(!decoded) {
+    if(!decoded || !decoded.userId) {
         ws.close();
         return;
     }
